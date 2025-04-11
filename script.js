b5043f08f4078bdf166c46eb67c691833d3ba08d
MAYUR AJIT SAHARE <mayursahare2024@gmail.com>
2025-04-11 14:48:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+// This file can hold common JS functions if you need them in multiple pages.
+// For now, our page-specific JS is included inline.
+console.log("Hospital Bed Management website loaded.");
+
